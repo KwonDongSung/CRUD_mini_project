10df338569e5c138baa2f146d07119be2e5b9d06
KwonDongSung <21700045@handong.edu>
2020-05-05 23:40:07 +0900

    changed some functions due to error

--- file.c
+++ file.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 #include "book.h"
 
-typedef struct{
-	char name[20];
-	char genre[20];
-	char author[20];
-	int price;
-	int star;
-}Book;
-
 int loadbook(Book *b){
-	FILE *fp = NULL;
-	fp = fopen("","r");
+	FILE *fp;
+	fp = fopen("bookstore.txt","r");
 	int i = 0;
 
 	if(fp==NULL){
@@ -19,21 +11,21 @@ int loadbook(Book *b){
 		return 0;
 	}
 	while(!feof(fp)){
-		fscanf(fp,"%d %d %s %s %[^\n]s\n",&b[i].price,&b[i].star,b[i].author,b[i].genre,b[i].name);
+		fscanf(fp,"%d %d %s %s %[^\n]\n",&b[i].price,&b[i].star,b[i].author,b[i].genre,b[i].name);
 		i++;
 	}
 	printf("\t[FILE LOADED]");
 	fclose(fp);
-	return i-1;
+	return i;
 }
 
 void savebook(Book *b, int count){
-	FILE *fp = NULL;
-	fp = fopen("","w");
+	FILE *fp;
+	fp = fopen("bookstore.txt","w");
 
 	for(int i = 0; i<count; i++){
 		if(b[i].star!=-1)
-			fprintf(fp,"%d %d %s %s %[^\n]s\n",b[i].price,b[i].star,b[i].author,b[i].genre,b[i].name);
+		fprintf(fp,"%d %d %s %s %s \n",b[i].price,b[i].star,b[i].author,b[i].genre,b[i].name);
 	}
 	
 	printf("\t[FILE SAVED]");
